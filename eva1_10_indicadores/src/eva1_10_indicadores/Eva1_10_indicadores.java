159b92e697e00da2f6e307228cd56924d89ecdb2
Dayana Yamileth Saldivar Martinez <143575706+DSSSqrew1260zxcm@users.noreply.github.com>
2023-09-14 16:18:58 -0600

    Add files via upload

---
+++ eva1_10_indicadores/src/eva1_10_indicadores/Eva1_10_indicadores.java
@@ -0,0 +1,22 @@
+package eva1_10_indicadores;
+
+public class Eva1_10_indicadores {
+
+    public static void main(String[] args) {
+        //int 1salario; no cumple con las reglas : inicia con nùmero
+        //int salario&; no cumple con las reglas: el & no es un caracter alfanumèrico
+        int mi_salario;
+        int $salario;
+        int salario100;
+        //int _; no es vàlido
+        int $;
+        //int mi salario; no es vàlido, espacio en blanco
+        int salario;
+        int Salario;
+        int SALARIO;
+        //int salario; no se puede repetir identificadores
+        //int public; no podemos usar keywords(palabras reservadas)
+ 
+    }
+    
+}
